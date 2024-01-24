2df82d262ef68c244093a64d03ce64bb54458e35
hugo de araujo <hugo.saraujo@tutanota.com>
2024-01-24 00:02:23 -0300

    Criação de um método para Adicionar filmes em uma lista.

---
+++ FilmesApi/Controllers/FilmeController.cs
@@ -0,0 +1,19 @@
+﻿using FilmesApi.Models;
+using Microsoft.AspNetCore.Mvc;
+
+namespace FilmesApi.Controllers;
+
+[ApiController]
+[Route("[controller]")]
+public class FilmeController
+{
+    private static List<Filme> filmes = new List<Filme>();
+
+    [HttpPost]
+    public void AdicionaFilme([FromBody] Filme filme)
+    {
+        filmes.Add(filme);
+        Console.WriteLine(filme.Titulo);
+        Console.WriteLine(filme.TempoDeDuracao);
+    }
+}
---
+++ FilmesApi/Models/Filme.cs
@@ -0,0 +1,16 @@
+﻿using System.ComponentModel.DataAnnotations;
+using System.Reflection.Metadata.Ecma335;
+
+namespace FilmesApi.Models;
+
+public class Filme
+{
+    [Required(ErrorMessage = "O título do filme é obrigatório")]
+    public string Titulo { get; set; }
+    [Required]
+    [Range(60, 600, ErrorMessage = "A duração do filme deve ser entre e 60 e 600")]
+    public int TempoDeDuracao { get; set; }
+    [Required(ErrorMessage = "O gênero do filme é obrigatório")]
+    [MaxLength(25, ErrorMessage = "O tamanho do gênero não pode exceder 25 caracteres")]
+    public string Genero { get; set; }
+}
