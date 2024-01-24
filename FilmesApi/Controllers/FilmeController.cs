cb29e02f7370b58259ee4831e4524c93f750fe4d
hugo de araujo <hugo.saraujo@tutanota.com>
2024-01-24 16:58:27 -0300

    Criação de métodos para retornar um filme ou uma lista de filmes menor.

--- FilmesApi/Controllers/FilmeController.cs
+++ FilmesApi/Controllers/FilmeController.cs
@@ -8,12 +8,27 @@ namespace FilmesApi.Controllers;
 public class FilmeController
 {
     private static List<Filme> filmes = new List<Filme>();
+    private static int Id = 0; 
 
     [HttpPost]
     public void AdicionaFilme([FromBody] Filme filme)
     {
+        filme.Id = Id++;
         filmes.Add(filme);
         Console.WriteLine(filme.Titulo);
         Console.WriteLine(filme.TempoDeDuracao);
+    }
+
+    [HttpGet]
+    public IEnumerable<Filme> PegaFilmes([FromQuery]int skip = 0, 
+        [FromQuery]int take = 30)
+    {
+        return filmes.Skip(skip).Take(take);
+    }
+
+    [HttpGet("{id}")]
+    public Filme? RecuperaFilme(int id)
+    {
+        return filmes.FirstOrDefault(filme => filme.Id == id);
     }
 }
