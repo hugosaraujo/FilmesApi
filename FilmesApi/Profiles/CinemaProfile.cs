58384d02dfe84179dee08036db269e888088c78b
hugo de araujo <hugo.saraujo@tutanota.com>
2024-01-26 20:59:56 -0300

    criação da cinema controller e da cinema profile

---
+++ FilmesApi/Profiles/CinemaProfile.cs
@@ -0,0 +1,15 @@
+﻿using AutoMapper;
+using FilmesApi.Dtos;
+using FilmesApi.Models;
+
+namespace FilmesApi.Profiles;
+
+public class CinemaProfile : Profile
+{
+    public CinemaProfile()
+    {
+        CreateMap<CreateCinemaDto, Cinema>();
+        CreateMap<Cinema, ReadCinemaDto>();
+        CreateMap<UpdateCinemaDto, Cinema>();
+    }
+}
