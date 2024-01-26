3c4764942899c3500ed2fbedfa4ea3e9b58fb94e
hugo de araujo <hugo.saraujo@tutanota.com>
2024-01-26 20:52:05 -0300

    Crescendo o projeto adicionando a categoria cinema e seus respectivos dtos

---
+++ FilmesApi/Dtos/CreateCinemaDto.cs
@@ -0,0 +1,9 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace FilmesApi.Dtos;
+
+public record CreateCinemaDto
+{
+    [Required(ErrorMessage = "O campo nome é obrigatório")]
+    public string Nome { get; set; }
+}
---
+++ FilmesApi/Dtos/ReadCinemaDto.cs
@@ -0,0 +1,9 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace FilmesApi.Dtos;
+
+public record ReadCinemaDto
+{
+    public int Id { get; set; }
+    public string Nome { get; set; }
+}
