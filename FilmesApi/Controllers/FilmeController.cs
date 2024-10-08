c606b59a42cd76fb6493fc664a039859e547904a
hugo de araujo <hugo.saraujo@tutanota.com>
2024-10-08 19:43:11 -0300

    Adicionar arquivos de projeto.

---
+++ FilmesApi/Controllers/FilmeController.cs
@@ -0,0 +1,19 @@
+﻿using FilmesApi.Models;
+using Microsoft.AspNetCore.Mvc;
+
+namespace FilmesApi.Controllers;
+
+[ApiController]
+[Route("[controller]")]
+public class FilmeController : ControllerBase
+{
+    private static List<Filme> _filmes = new();
+
+    [HttpPost]
+    public void AdicionaFilme([FromBody] Filme filme)
+    {
+        _filmes.Add(filme);
+        Console.WriteLine(filme.Titulo);
+        Console.WriteLine(filme.Duracao);
+    }
+}
---
+++ FilmesApi/Models/Filme.cs
@@ -0,0 +1,15 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace FilmesApi.Models;
+
+public class Filme
+{
+    [Required(ErrorMessage = "O Título do filme é obrigatório")]
+    public string? Titulo { get; set; }
+    [Required(ErrorMessage = "O Gênero do filme é obrigatório")]
+    [MaxLength(50, ErrorMessage = "O Gênero não pode ter mais que 50 caracteres")]
+    public string? Genero { get; set; }
+    [Required]
+    [Range(70, 600, ErrorMessage = "O filme deve ter uma duração entre 70 e 600 minutos")]
+    public int Duracao { get; set; }
+}
